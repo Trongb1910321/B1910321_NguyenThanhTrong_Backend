f17215c0c18ac4d8f3ff8911bc22beaf554737d3
Trongb1910321 <114137576+Trongb1910321@users.noreply.github.com>
2023-01-14 19:20:21 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const cors = require("cors");
 
+const contactRouter = require("./app/routes/contact.route")
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to contact book application." });
 });
+app.use("/api/contacts", contactRouter);
+
 
 module.exports = app;
+
